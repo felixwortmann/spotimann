b4fde0a70caaaddb4d133ce50ff541b7d1f0910b
Tim Grohmann <timgrohmann@icloud.com>
2020-11-02 18:50:59 +0100

    added login functionality to frontend

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  Joa nichts weiter
+  <br>
+  <Login></Login>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
---
+++ frontend-vue/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <p v-if="auth.state.loggedIn">Willkommen, {{auth.state.name}}</p>
+    <button v-if="auth.state.loggedIn" @click="auth.logout">Abmelden</button>
+    <button v-else @click="auth.login">Anmelden</button>
+</div>
+</template>
+
+<script>
+import auth from '../service/Authentication'
+
+export default {
+    data() {
+        return {
+            auth: auth
+        }
+    },
+    created() {
+        console.log("Created Login Button");
+        console.log(auth.getToken());
+    },
+    methods: {
+        login() {
+            console.log("Login");
+            auth.login();
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
