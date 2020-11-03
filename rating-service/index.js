fa168a7331ace1f30d9f9cea18f42be9e4f48db8
Niclas Kaufmann <kaufmann.niclas@web.de>
2020-11-03 13:17:38 +0100

    updated rating service

--- rating-service/index.js
+++ rating-service/index.js
@@ -3,40 +3,64 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser')
 const jwkToPem = require('jwk-to-pem');
 const jsonwebtoken = require('jsonwebtoken');
-const request = require('request');
+const axios = require('axios');
 
+// variable to store the pem (Privacy-Enhanced Mail)
 let pem;
 
-// Request keycloak token
-request('http://localhost:8180/auth/realms/spotimann/protocol/openid-connect/certs', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-	pem = jwkToPem(body.keys[0]);
-});
-
-const app = express();
+// constant to store the port
 const port = 8080;
 
-// Middleware auth
-app.use(function (req, res, next) {
-	const token = req.get('Authorization').slice(7);
-	try {
-		const parsedToken = jsonwebtoken.verify(token, pem);
-		req.principal = parsedToken;
-		next();
-	} catch (err) {
-		res.status(403).send('Invalid credentials')
-	}
-	
-});
-
 // Database information
-const url = 'mongodb://localhost:2000';
+const url = 'mongodb://localhost:27018';
 const dbName = 'ratings';
 const dbCollection = 'ratings';
 
-// Express
+// Express initiali
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware auth
+app.use(function (req, res, next) {
+	// check if token exists and remove 'Bearer '
+	const token = (req.get('Authorization') && req.get('Authorization').slice(7)) || null;
+
+	// check if pem exists, if not, call `getKeyCloakToken` to get the pem. Afterwards validate the token
+	if (!pem) {
+		getKeyCloakToken(res).then(_ => {
+			try {
+				const parsedToken = jsonwebtoken.verify(token, pem);
+				req.principal = parsedToken;
+				next();
+			} catch (err) {
+				console.warn(err);
+				res.status(403).send('Invalid credentials')
+			}
+		});
+		// if pem already exists, validate the token
+	} else {
+		try {
+			const parsedToken = jsonwebtoken.verify(token, pem);
+			req.principal = parsedToken;
+			next();
+		} catch (err) {
+			console.warn(err);
+			res.status(403).send('Invalid credentials');
+		}
+	}
+});
+
+// Function to request keycloak token
+function getKeyCloakToken(res) {
+	return axios.get('http://localhost:8180/auth/realms/spotimann/protocol/openid-connect/certs').then(response => {
+		pem = jwkToPem(response.data.keys[0]);
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(500).send('Can not connect to authentication service');
+	});
+}
 
 // Get ratings for song id
 app.get('/:id', (req, res) => {
@@ -155,14 +179,26 @@ app.post('/:id', (req, res) => {
 					if (result.modifiedCount) {
 						res.status(201).send('Rating was created')
 					} else {
-						console.log(err);
-						res.status(500).send('Failed to process query');
+						// Insert new song entity with rating
+						collection.insertOne({
+							'songID': songID,
+							'ratings': [
+								newRating
+							]
+						}, (err, _) => {
+							if (err) {
+								console.log(err);
+								res.status(500).send('Failed to process query');
+								return;
+							}
+							res.status(201).send('Rating was created')
+						});
 					}
+
+					// Close database connection
+					client.close();
 				});
 			}
-
-			// Close database connection
-			client.close();
 		});
 	});
 });
