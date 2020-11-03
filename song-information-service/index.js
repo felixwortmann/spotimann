2b6e162a4ae9c642108f9275031ea1a37930a2ff
Florian Redmann <Florian.Redmann@rewe-group.com>
2020-11-03 15:49:25 +0100

    Added keycloak to song information

--- song-information-service/index.js
+++ song-information-service/index.js
@@ -1,25 +1,68 @@
 const express = require('express');
-const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const jwkToPem = require('jwk-to-pem');
+const jsonwebtoken = require('jsonwebtoken');
+const axios = require('axios');
 
+// Server stuff
 const app = express();
-app.use(cors());
 const port = 8080;
  
-// Connection URL
+// Database stuff
 const url = 'mongodb://database-song-information-service:27017';
- 
-// Database Name
 const dbName = 'information';
 
+// variable to store the pem (Privacy-Enhanced Mail)
+let pem;
+
+// Middleware auth
+app.use(function (req, res, next) {
+	// Check if token exists and remove 'Bearer'
+	const token = (req.get('Authorization') && req.get('Authorization').slice(7)) || null;
+	console.log(token);
+
+	// Check if pem exists, if not call `getKeyCloakToken` to get the pem. Afterwards validate the token
+	if (!pem) {
+		getKeyCloakToken(res).then(_ => {
+			try {
+				const parsedToken = jsonwebtoken.verify(token, pem);
+				next();
+			} catch (err) {
+				console.warn(err);
+				res.status(403).send('Invalid credentials')
+			}
+		});
+		// If pem already exists, validate the token
+	} else {
+		try {
+			const parsedToken = jsonwebtoken.verify(token, pem);
+			next();
+		} catch (err) {
+			console.warn(err);
+			res.status(403).send('Invalid credentials');
+		}
+	}
+});
+
+// Function to request keycloak token
+function getKeyCloakToken(res) {
+	return axios.get('http://keycloak:8080/auth/realms/spotimann/protocol/openid-connect/certs').then(response => {
+		pem = jwkToPem(response.data.keys[0]);
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(500).send('Cannot connect to authentication service');
+	});
+}
+
+// Get song list
 app.get('/songs', (req, res) => {
 	MongoClient.connect(url, function(err, client) {
 		if (err) {
 			res.status(500).send('Could not connect to server');
 		 	return;
 		}
-	 
+
 		const db = client.db(dbName);
 		const collection = db.collection('songs');
 
@@ -34,6 +77,7 @@ app.get('/songs', (req, res) => {
 	});
 })
  
+// Get song with ID
 app.get('/songs/:id', (req, res) => {
 	const id = req.params.id;
 	MongoClient.connect(url, function(err, client) {
@@ -41,7 +85,7 @@ app.get('/songs/:id', (req, res) => {
 			res.status(500).send('Could not connect to server');
 		 	return;
 		}
-	 
+
 		const db = client.db(dbName);
 		const collection = db.collection('songs');
 
